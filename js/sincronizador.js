bc57d126185323ef732f6a7ace0b546eebed697b
miguelm197 <miguelmax197@gmail.com>
2017-08-09 21:44:40 -0300

    f

--- js/sincronizador.js
+++ js/sincronizador.js
@@ -20,7 +20,11 @@ function Tipo(id, seccion, tipo) {
     this.tipo = tipo;
 }
 
-
+function Relacion(id, sector, tipo) {
+    this.id = id;
+    this.sector = sector;
+    this.tipo = tipo;
+}
 
 
 var imagenes = "./img/productos/";
@@ -28,12 +32,14 @@ var urlArchivo = "./datos/";
 var archivoProductos = urlArchivo + "productos.csv";
 var archivoSecciones = urlArchivo + "secciones.csv";
 var archivoTipos = urlArchivo + "tipos.csv";
+var archivoRelaciones = urlArchivo + "relaciones.csv";
 
 
 var productos = [];
 var tipos = [];
 var secciones = [];
 var carrito = [];
+var relaciones = [];
 
 var idProdVistaPrevia = 0;
 var vistaProductoActiva = true;
@@ -72,11 +78,32 @@ var cargarTipos = function (lista) {
     }
 }
 
+var cargarRelaciones = function (lista) {
+    var coleccion = [];
+
+    console.log(lista);
+    for (var l = 0; l < lista.length; l++) {
+        var idProducto = lista[l].id;
+        var idTipo = lista[l].tipo;
+
+        var objProd = consultaProducto(idProducto);
+        var objTipo = consultaTipo(idTipo);
+        var objSecc = objTipo.seccion;
+
+        var relacion = new Relacion(objProd, objSecc, objTipo);
+
+        relaciones.push(relacion);
+    }
+
+}
+
+
 
 $(document).ready(function () {
     leer(archivoProductos, ",", cargarProductos);
     leer(archivoSecciones, ";", cargarSecciones);
     leer(archivoTipos, ";", cargarTipos);
+    leer(archivoRelaciones, ";", cargarRelaciones);
 
     setTimeout(function cargarTipo() {
         cargarTablaProductos(productos)
@@ -160,9 +187,13 @@ function cargarTablaProductos(lista) {
         var descuento = lista[i].descuento;
         var imagen = imagenes + lista[i].imagen;
 
-        var item = "";
+        var visibilidad = "visible";
 
-        var parte1 = `
+        if (descuento <= 0) {
+            visibilidad = "invisible";
+        }
+
+        var item = `
         <div class="articulo col s12 m10 offset-m1 l3">
             <div class="row arriba">
                 <div class="izquierda col s4 m6 l6">
@@ -170,23 +201,22 @@ function cargarTablaProductos(lista) {
                         <div class="anterior">ANTES</div>
                         <div class="moneda">$</div>
                         <div class="monto">`+ precioViejo + `</div>
-                    </div>`
-        var descuentoHtml = `
-                    <div class="descuento">
+                    </div>
+                    <div class="descuento `+ visibilidad + `">
                         <div class="monto inline">`+ descuento + `</div>
                         <div class="porcentaje inline">%</div>
                         <div class="off">OFF</div>
-                    </div>`
-        var parte2 = `
+                    </div>
                     <div class="ahora">
                         <div class="ahorita">AHORA</div>
                             <div class="moneda inline b">$</div>
                             <div class="monto inline b">`+ precio + `</div>
                         </div>
                         <div class="vermas" onclick="verProducto(`+ id + `)">Ver más</div>
+                        <div class="btn agrAlCarrito" onclick="agregarItmAlCarrito(`+ id + `)">Agregar</div>
                     </div>
                     <div class="derecha col s8 m6 l6">
-                        <div class='imagen' style='background-image: url("`+ imagen + `");'></div>
+                        <div class='imagen' onclick="verProducto(`+ id + `)" style='background-image: url("` + imagen + `");'></div>
                     </div>
                 </div>
                 <div class="row abajo">
@@ -195,16 +225,10 @@ function cargarTablaProductos(lista) {
             </div>
         </div>`
 
-
-        item = item + parte1;
-        if (descuento > 0) {
-            item = item + descuentoHtml;
-        }
-        item = item + parte2;
-        articulos = articulos + item;
+        $(".contenedor-articulos").append(item);
     }
-    articulos = articulos
-    $(".contenedor-articulos").append(articulos);
+
+
 }
 
 function cargarTablaMenu() {
@@ -212,12 +236,13 @@ function cargarTablaMenu() {
     for (var s = 0; s < secciones.length; s++) {
         var seccionT = secciones[s].seccion;
         var sector = secciones[s].id;
-        menu = menu + `<li>\n<div class="collapsible-header">` + seccionT + `</div>\n`
+        menu = menu + `<li>\n<div class="collapsible-header" onclick="consultarPorSeccion(` + sector + `)">` + seccionT + `</div>\n`
         for (var t = 0; t < tipos.length; t++) {
             var seccion = tipos[t].seccion.id;
             if (seccion == sector) {
                 var tipo = tipos[t].tipo;
-                menu = menu + `<div class="collapsible-body"><a><span>-  ` + tipo + `</span></a></div></div>\n`
+                var idTipo = tipos[t].id;
+                menu = menu + `<div class="collapsible-body"><a onclick="consultarPorTipo(` + idTipo + `)"><span>-  ` + tipo + `</span></a></div></div>\n`
             }
         }
         menu = menu + "</li>\n"
@@ -247,6 +272,7 @@ function verProducto(idProducto) {
     var precioViejo;
     var descuento;
     var imagen;
+    var cantidad;
 
     for (var p = 0; p < productos.length; p++) {
         var id = productos[p].id;
@@ -257,19 +283,23 @@ function verProducto(idProducto) {
             precioViejo = productos[p].precioViejo;
             descuento = productos[p].descuento;
             imagen = productos[p].imagen;
-            console.log("***************************************");
-            console.log(nombre)
-            console.log(descripcion)
-            console.log(precio)
-            console.log(descuento)
-            console.log(imagen)
-            console.log("***************************************");
+            cantidad = consultarCantProducto(id);
+
+            // console.log("***************************************");
+            // console.log(nombre)
+            // console.log(descripcion)
+            // console.log(precio)
+            // console.log(descuento)
+            // console.log(imagen)
+            // console.log(cantidad)
+            // console.log("***************************************");
 
             $("#nombreVP").html(nombre);
             $("#descripcionVP").html(descripcion);
             $("#precioViejoVP").html(precioViejo);
             $("#descuentoVP").html(descuento);
             $("#precioVP").html(precio);
+            $("#cantidadArticulo").val(cantidad);
             $("#imagenVP").css('background-image', 'url("./img/productos/' + imagen + '")');
 
             activarVistaProductos();
@@ -284,10 +314,13 @@ var Itm = function (key, cantidad) {
     this.cantidad = cantidad;
 }
 
-
 function agregarAlCarrito() {
+    agregarItmAlCarrito(idProdVistaPrevia);
+}
+
+function agregarItmAlCarrito(id) {
     var cantidad = 1;
-    var idProducto = idProdVistaPrevia;
+    var idProducto = id;
     var obj = new Itm(idProducto, cantidad);
 
     if (existenciaProducto(idProducto)) {
@@ -298,12 +331,37 @@ function agregarAlCarrito() {
     }
     console.log(carrito);
     actualizarCarrito()
+
+
+    cantidad = consultarCantProducto(id);
+    $("#cantidadArticulo").val(cantidad);
+
 }
 
+function eliminarDelCarrito(id) {
 
+    var pos = consultaIndiceItmCarrito(id);
+    carrito.splice(pos, 1);
+
+    actualizarCarrito();
+}
+
+function agregarCantidadProducto() {
+    var id = idProdVistaPrevia;
+    var cant = $("#cantidadArticulo").val();
+
+    for (var p = 0; p < carrito.length; p++) {
+        var idProdCarrito = carrito[p].key;
+        if (id == idProdCarrito) {
+            carrito[p].cantidad = cant;
+        }
+    }
+    actualizarCarrito();
+}
 
 function actualizarCarrito() {
     var lista = "";
+    var totalCarrito = 0;
 
     for (var c = 0; c < carrito.length; c++) {
         var id = carrito[c].key;
@@ -321,6 +379,7 @@ function actualizarCarrito() {
                 <div class="abajo">
                     <div class="renglonUp">
                         <div class="nombre">`+ nombre + `</div>
+                        <a class="eliminar" onclick="eliminarDelCarrito(`+ id + `)">Eliminar</a>
                     </div>
                     <div class="renglonDown">
                         <div class="monedaU inline">$</div>
@@ -335,10 +394,56 @@ function actualizarCarrito() {
             </div>
         `
         lista = lista + item;
+        totalCarrito = totalCarrito + total;
     }
     $(".contenedor-carrito").html(lista);
+
+    $("#totalCarrito").html("Total $" + totalCarrito);
+
+
+    if (consultaCantidadProductosEnCarrito() == 0) {
+        $("#totalCarrito").html("El carrito está vacío");
+
+    }
 }
 
+function limpiarGondola() {
+    $(".contenedor-articulos").html("");
+
+}
+
+
+
+
+
+function consultarPorTipo(tipo) {
+    var consulta = [];
+    for (var r = 0; r < relaciones.length; r++) {
+        var idTipo = relaciones[r].tipo.id;
+
+        if (idTipo == tipo) {
+            var idProd = relaciones[r].id.id;
+            var prod = consultaProducto(idProd);
+            consulta.push(prod);
+        }
+    }
+    limpiarGondola();
+    cargarTablaProductos(consulta);
+}
+function consultarPorSeccion(seccion) {
+    var consulta = [];
+    for (var r = 0; r < relaciones.length; r++) {
+        var idSeccion = relaciones[r].tipo.seccion.id
+
+        if (idSeccion == seccion) {
+            var idProd = relaciones[r].id.id;
+            var prod = consultaProducto(idProd);
+            consulta.push(prod);
+        }
+    }
+    limpiarGondola();
+    cargarTablaProductos(consulta);
+}
 
 
 
@@ -359,7 +464,17 @@ function aumentarCantProducto(id, valor) {
         var idProdCarrito = carrito[p].key;
         if (id == idProdCarrito) {
             var cant = carrito[p].cantidad;
-            carrito[p].cantidad = cant + valor;
+            carrito[p].cantidad = parseInt(cant) + parseInt(valor);
+        }
+    }
+}
+
+function consultarCantProducto(id) {
+    for (var p = 0; p < carrito.length; p++) {
+        var idProdCarrito = carrito[p].key;
+        if (id == idProdCarrito) {
+            var cant = carrito[p].cantidad;
+            return cant;
         }
     }
 }
@@ -370,6 +485,43 @@ function consultarPropiedadProducto(id, propiedad) {
         if (idProd == id) {
             var valor = productos[p][propiedad];
             return valor;
+        }
+    }
+}
+
+function consultaIndiceItmCarrito(id) {
+    if (existenciaProducto(id)) {
+
+        for (var p = 0; p < carrito.length; p++) {
+            var idProdCarrito = carrito[p].key;
+            if (id == idProdCarrito) {
+                return p;
+            }
+        }
+
+    }
+}
+
+function consultaCantidadProductosEnCarrito() {
+    var cantidad = carrito.length;
+    return cantidad;
+}
+
+function consultaProducto(id) {
+
+    for (var p = 0; p < productos.length; p++) {
+        idProd = productos[p].id;
+        if (idProd == id) {
+            return productos[p];
+        }
+    }
+}
+
+function consultaTipo(id) {
+    for (var t = 0; t < tipos.length; t++) {
+        idTipo = tipos[t].id;
+        if (idTipo == id) {
+            return tipos[t];
         }
     }
 }
